b5744751ff530a56f1d450bb54d65ee6b2feeaf2
serii <seriiburduja@gmail.com>
2020-07-31 13:32:21 +0300

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, {Component} from 'react'
 import './App.scss'
-import {Route} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
 
 class App extends Component {
-  render() {
+	render() {
 
-    return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-          </ul>
-        </nav>
+		return (
+			<div>
+				<nav className="nav">
+					<ul>
+						<li>
+							<NavLink to="/">Home</NavLink>
+						</li>
+						<li>
+							<NavLink to="/about">About</NavLink>
+						</li>
+						<li>
+							<NavLink to="/cars">Cars</NavLink>
+						</li>
+					</ul>
+				</nav>
 
-        <hr/>
+				<hr/>
 
-        {/*localhost:3000*/}
-        <Route path="/" exact render={() => <h1>Home Page</h1>} />
-
-        <About />
-
-        <Cars />
-      </div>
-    );
-  }
+				{/*localhost:3000*/}
+				<Route path="/" exact render={() => <h1>Home Page</h1>}/>
+				<Route path="/about" component={About}/>
+				<Route path="/cars" component={Cars}/>
+			</div>
+		);
+	}
 }
 
 export default App
