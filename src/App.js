621cc5644f9f2c53155ca7f0abbf209d5c3f591e
serii <seriiburduja@gmail.com>
2020-07-31 12:54:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {NavLink, Route, Switch} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                exact
+                activeClassName={'wfm-active'}
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{
+                color: 'blue'
+              }}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={{
+                pathname: '/cars/'
+              }}>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <hr/>
+
+        {/*localhost:3000*/}
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home Page</h1>}/>
+          <Route path="/about" component={About}/>
+          <Route path="/cars/:name" component={CarDetail}/>
+          <Route path="/cars" component={Cars}/>
+        </Switch>
+
+
+      </div>
+    );
+  }
+}
+
+export default App
